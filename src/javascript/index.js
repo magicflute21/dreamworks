46ca28140b73e86907cbdc8778b073430c712144
Triin <triin.maesepp@websystems.ee>
2020-09-22 15:23:55 +0300

    first commit

---
+++ src/javascript/index.js
@@ -0,0 +1,32 @@
+import '../scss/main.scss';
+
+
+
+// function a (){
+
+
+function a (){
+
+    function b(){
+        console.log(myVar);
+    }
+
+
+    var myVar = 2;
+    b();
+}
+
+
+
+function a (){
+
+    function b(){
+        console.log(myVar);
+    }
+
+    var myVar = 2;
+    b();
+}
+
+var myVar = 1;
+a();
